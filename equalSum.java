790e495ac8d8aaa979789cea9a701d6be11a05ba
T-Asare <77137510+T-Asare@users.noreply.github.com>
2021-01-17 04:00:05 +0000

    Add files via upload

---
+++ equalSum.java
@@ -0,0 +1,11 @@
+public class  equalSum {
+    public static boolean hasEqualSum(int num1, int num2, int num3) {
+        if (num1 + num2 == num3)
+            return (true);//checks requirements for all three numbers
+        return false;
+    }
+
+  /*  public static void main(String[] args) { //commented out for purposes of assignment requirements. Please uncomment if you dont have main class
+        System.out.println(hasEqualSum(1, 1, 2));
+    }*/
+}
