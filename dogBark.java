8cfb363b928c9e36e6c2727ff0bb3277d89ebf20
T-Asare <77137510+T-Asare@users.noreply.github.com>
2021-01-18 04:19:43 +0000

    Update dogBark.java

--- dogBark.java
+++ dogBark.java
@@ -1,8 +1,8 @@
 public class dogBark {
     public static boolean shouldWakeUp(boolean barking, int timeOfTheDay){
        if( timeOfTheDay>=0 && timeOfTheDay<24)
-            if (barking) //this statement can also be written as if summer == true checks if it is summer time
-                return timeOfTheDay <=8 || timeOfTheDay >= 22; // the range is between 25 and 45
+            if (barking) //this statement can also be written as if barking == true checks if it is summer time
+                return timeOfTheDay <=8 || timeOfTheDay >= 22; // the range is for times before 8AM and times after 10PM
             //return  false;
             else
              return false;
